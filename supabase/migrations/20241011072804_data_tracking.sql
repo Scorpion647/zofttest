73bd1c265b297157d766de26105c70d8e243a86f
pablohernandezm <phernandezm07@gmail.com>
2025-08-09 05:12:26 -0500

    fix: format

--- supabase/migrations/20241011072804_data_tracking.sql
+++ supabase/migrations/20241011072804_data_tracking.sql
@@ -27,64 +27,46 @@ CREATE OR REPLACE FUNCTION track_bill (
   clean_bill BOOLEAN DEFAULT FALSE,
   clean_data BOOLEAN DEFAULT FALSE
 ) returns void AS $$
-    declare
-        _base_bill public.base_bills%rowtype;
-        _material public.materials%rowtype;
-        _supplier public.suppliers%rowtype;
-        _supplier_data_record record;
+DECLARE
+    _base_bill public.base_bills%rowtype;
+    _material public.materials%rowtype;
+    _supplier public.suppliers%rowtype;
+    _supplier_data_record record;
 BEGIN
-    select * into _base_bill from public.base_bills where base_bill_id = $1;
-    select * into _material from public.materials where material_code = _base_bill.material_code;
-    select * into _supplier from public.suppliers where supplier_id = _base_bill.supplier_id;
-
-    for _supplier_data_record in select * from public.supplier_data where base_bill_id=$1 loop
-        insert into public.data_tracking (
-            purchase_order,
-            items,
-            code,
-            description,
-            quantity,
-            bill_measurement_unit,
-            supplier_name,
-            bill_number,
-            fmm,
-            subheading,
-            material_measurement_unit,
-            trm,
-            bill_unit_price,
-            bill_total_price,
-            data_unit_price,
-            data_total_price,
-            type,
-            gross_weight,
-            packages
-        ) values (
-            _base_bill.purchase_order,
-            _base_bill.item,
-            _material.material_code,
-            _base_bill.description,
-            _supplier_data_record.billed_quantity,
-            _base_bill.measurement_unit,
-            _supplier.name,
-            _supplier_data_record.bill_number,
-            'To define',
-            _material.subheading,
-            _material.measurement_unit,
-            _supplier_data_record.trm,
-            _base_bill.unit_price,
-            _base_bill.net_price,
-            _supplier_data_record.billed_unit_price,
-            _supplier_data_record.billed_total_price,
-            _material.type,
-            _supplier_data_record.gross_weight,
-            _supplier_data_record.packages
-        );
-    end loop;
-
-    if (clean_bill) then
-        delete from public.base_bills where base_bill_id=$1;
-    elseif(clean_data) then
-        delete from public.supplier_data where base_bill_id=$1;
-    end if;
+    SELECT
+        * INTO _base_bill
+    FROM
+        public.base_bills
+    WHERE
+        base_bill_id = $1;
+    SELECT
+        * INTO _material
+    FROM
+        public.materials
+    WHERE
+        material_code = _base_bill.material_code;
+    SELECT
+        * INTO _supplier
+    FROM
+        public.suppliers
+    WHERE
+        supplier_id = _base_bill.supplier_id;
+    FOR _supplier_data_record IN
+    SELECT
+        *
+    FROM
+        public.supplier_data
+    WHERE
+        base_bill_id = $1 LOOP
+            INSERT INTO public.data_tracking (purchase_order, items, code, description, quantity, bill_measurement_unit, supplier_name, bill_number, fmm, subheading, material_measurement_unit, trm, bill_unit_price, bill_total_price, data_unit_price, data_total_price, type, gross_weight, packages)
+                VALUES (_base_bill.purchase_order, _base_bill.item, _material.material_code, _base_bill.description, _supplier_data_record.billed_quantity, _base_bill.measurement_unit, _supplier.name, _supplier_data_record.bill_number, 'To define', _material.subheading, _material.measurement_unit, _supplier_data_record.trm, _base_bill.unit_price, _base_bill.net_price, _supplier_data_record.billed_unit_price, _supplier_data_record.billed_total_price, _material.type, _supplier_data_record.gross_weight, _supplier_data_record.packages);
+        END LOOP;
+    IF (clean_bill) THEN
+        DELETE FROM public.base_bills
+        WHERE base_bill_id = $1;
+        elseif (clean_data) THEN
+        DELETE FROM public.supplier_data
+        WHERE base_bill_id = $1;
+    END IF;
 END
 $$ language plpgsql;
